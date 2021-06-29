042ca9d7a6355ddfc9bf7f457026304aa7da5006
Amir Maleki <amir.maleki.zamenjani@gmail.com>
2021-06-29 00:13:35 -0400

    small fix in test

--- code/test/test_area.py
+++ code/test/test_area.py
@@ -20,14 +20,14 @@ class MeshTest(unittest.TestCase):
     def test_compute_mesh_area(self):
         simple_mesh = self.read_mesh("simple_mesh.obj")
         self.assertAlmostEqual(compute_mesh_area(simple_mesh), simple_mesh.area)
-        self.ass
+
         # complex_mesh = self.read_mesh("complex_mesh.obj")
         # self.assertAlmostEqual(compute_mesh_area(complex_mesh), complex_mesh.area)
 
     def test_compute_mesh_area_parallel(self):
         simple_mesh = self.read_mesh("simple_mesh.obj")
         self.assertAlmostEqual(compute_mesh_area_parallel(simple_mesh, n_jobs=4), simple_mesh.area)
-    #
+
     #     complex_mesh = self.read_mesh("complex_mesh.obj")
     #     self.assertAlmostEqual(compute_mesh_area_parallel(complex_mesh, n_jobs=4), complex_mesh.area)
 
